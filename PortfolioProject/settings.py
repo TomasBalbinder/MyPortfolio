f0080c2d148435f452ec46c08bef7d25bb9e1405
Tomas <tomasbalbinder@gmail.com>
2022-09-16 15:48:38 +0000

    from website, moved static file, change gitignore

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&(#7rcn=@802(u+f5gadvy5wo-!l!_!!(5^87b$oq+f$c$n(m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snowkillcz.pythonanywhere.com']
 
 
 # Application definition
@@ -122,9 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_URL = '/pictures/'
+STATIC_ROOT = '/home/SnowkillCZ/MyPortfolio/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/home/SnowkillCZ/MyPortfolio/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
